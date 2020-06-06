998109575d2a856a34519937ceec8c7b50faeac7
Todd Spainhour <toddspainhour@gmail.com>
2020-06-06 10:54:07 -0500

    basic reactstrap navbar working

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -6,19 +20,60 @@ import 'firebase/auth';
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
   logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
-  render() {
-    return (
-      <div className="MyNavbar">
-        <h1>your navbar component</h1>
-        <button className="btn btn-danger" onClick={this.logMeOut}>Logout</button>
-      </div>
-    );
-  }
+toggle = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  const { isOpen } = this.state;
+  return (
+    <div className="MyNavbar">
+      {/* <h1>your navbar component</h1> */}
+      {/* <button className="btn btn-danger" onClick={this.logMeOut}>Logout</button> */}
+      <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
+    </div>
+  );
+}
 }
 
 export default MyNavbar;
