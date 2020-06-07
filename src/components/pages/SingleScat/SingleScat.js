223490710f13df662057ca9e7e217d36fb7cfe40
Todd Spainhour <toddspainhour@gmail.com>
2020-06-06 22:44:27 -0500

    single view functional

--- src/components/pages/SingleScat/SingleScat.js
+++ src/components/pages/SingleScat/SingleScat.js
@@ -1,11 +1,32 @@
 import React from 'react';
+
 import './SingleScat.scss';
+import scatsData from '../../../helpers/data/scatsData';
 
 class SingleScat extends React.Component {
+  state = {
+    scat: [],
+  }
+
+  componentDidMount() {
+    const { scatId } = this.props.match.params;
+    scatsData.getSingleScat(scatId)
+      .then((response) => this.setState({ scat: response.data }))
+      .catch((err) => console.error('unable to get single scat', err));
+  }
+
   render() {
+    const { scat } = this.state;
     return (
-      <div className="SingleScat">
-        <h3>Single Scat</h3>
+      <div className="SingleScat" style={{ backgroundColor: scat.color }}>
+        <h3>{scat.location}</h3>
+    <p>Shape: {scat.shape}</p>
+    <p>Size: {scat.size}</p>
+    <p>Temperature: {scat.temperature}</p>
+    <p>Viscosity: {scat.viscosity}</p>
+    <p>Location: {scat.location}</p>
+    <p>Notes: {scat.notes}</p>
+    <p>Was it fulfilling? {scat.wasFulfilling ? 'Yes' : 'No'}</p>
       </div>
     );
   }
